c81ec84343b66557833b5e7f3c17b238c4c31310
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2017-08-13 19:22:31 -0400

    added material design and basic navigation structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
+import {MdButtonModule, MdIconModule, MdSidenavModule, MdToolbarModule} from "@angular/material";
 
 @NgModule({
 	declarations: [
 		AppComponent
 	],
 	imports: [
-		BrowserModule
+		BrowserModule, MdToolbarModule, MdButtonModule, MdIconModule, MdSidenavModule
 	],
 	providers: [],
 	bootstrap: [AppComponent]
