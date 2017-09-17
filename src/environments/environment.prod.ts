cd388b2a366035036d2efed883449a35665cc9b4
Nicolas Mongrain-Lacombe <nicolas.m.lacombe@gmail.com>
2017-09-17 01:29:49 -0400

    fixed prod vault-ws url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
 	production: true,
 	apiBaseUrls: {
 		userWs: 'https://user-api.nlacombe.net',
-		vaultWs: 'https://vault-api.nlacombe.net'
+		vaultWs: 'https://vault-api.nlacombe.net/api'
 	}
 };
